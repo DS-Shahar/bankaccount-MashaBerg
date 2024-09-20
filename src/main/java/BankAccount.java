02200cf2cd9a6967717e381f1f99224460d5dcc3
MashaBerg <mashabregman1974@gmail.com>
2024-09-20 20:24:42 +0300

    Add files via upload

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,80 @@
+public class BankAccount {
+	
+	 private int balance;
+	 private int minBalance = -5000;
+	 private String owner;
+	    
+	    /**
+	     * @param balance Amount of money in the account, can be negative
+	     * @param balance Must be >= minBalance
+	     */
+
+	    public BankAccount (int balance, int minBalance, String owner)
+	    {
+	    	this.balance = balance;
+	    	this.minBalance = minBalance;
+	    	this.owner = owner;
+	    }
+	    
+	    public boolean withdraw(int amount) {
+	        if (balance - amount < minBalance)
+	        {
+	        	 System.out.println("\n" + "Not enough money in balance!");
+	        	 return false;
+	        }
+	        
+	        balance = balance - amount;
+	        return true;
+	    }
+	    
+	    /**
+	     * @return true on success
+	     */
+	     public boolean transfer(BankAccount target, int amount) {
+	         if (withdraw(amount) == false)
+	         {
+	        	
+	             return false;
+	         }
+	         target.deposit(amount);
+	         return true;
+	     }
+	     
+	     public String toString() {
+	         return "balance of " + owner + ": " + balance;
+	     }
+
+	        
+	    public void deposit(int a) {
+	        balance = balance + a;
+	    }
+	    
+	    public int getBalance() {
+	        return balance;
+	    }
+	    
+		public int getMinBalance() {
+			return minBalance;
+		}
+		
+		public void setMinBalance(int minBalance) {
+			this.minBalance = minBalance;
+		}
+		
+		public void setBalance(int x) {
+			if (x >= minBalance)
+			{
+				balance = x;
+			}
+		}
+
+		public String getOwner() {
+			return owner;
+		}
+
+		public void setOwner(String owner) {
+			this.owner = owner;
+		}
+		
+		
+}
